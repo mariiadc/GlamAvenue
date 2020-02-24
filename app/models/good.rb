5dd451e2d37fcda1b0063173a3f569d2be992f72
Ines Marmelo <ines.c.marmelo@gmail.com>
2020-02-24 18:09:30 +0000

    Model and controller good created. def new is not saving yet

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,5 +1,7 @@
 class Good < ApplicationRecord
-  belongs_to :user
-  has_many :bookings
-  has_many :reviews, through :bookings
+  belongs_to :user # posts in your item
+  # has_many :bookings
+  # has_many :users, through :bookings
+  # bookings someone's item
+  # has_many :reviews, through :bookings
 end
