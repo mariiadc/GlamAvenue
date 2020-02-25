424ca25a4f57d61b9625b91a507074379be56487
Francisco Rosa <chicorosa@gmail.com>
2020-02-25 16:18:24 +0000

    Updated bookings schema. Added calendar to bookings form with flatpickr.js

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,14 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+import moment from "moment"
+
+flatpickr("#start_date", {
+  altInput: true,
+  "plugins": [new rangePlugin({ input: "#end_date"}) ],
+   minDate: moment().format("YYYY-MM-DD")
+
+
+});
+
+console.log(moment().format("YYYY-MM-DD"))
