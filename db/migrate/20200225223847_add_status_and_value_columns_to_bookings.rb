d6373ec9b445a885180a7b82dd79335ef06f2808
Mariia Campos <mariia.dcampos@gmail.com>
2020-02-26 13:41:56 +0000

    fixed bugs

--- db/migrate/20200225223847_add_status_and_value_columns_to_bookings.rb
+++ db/migrate/20200225223847_add_status_and_value_columns_to_bookings.rb
@@ -2,5 +2,5 @@ class AddStatusAndValueColumnsToBookings < ActiveRecord::Migration[5.2]
   def change
     add_column :bookings, :value, :integer, null: false
     add_column :bookings, :status, :boolean, default: false
-
+  end
 end
