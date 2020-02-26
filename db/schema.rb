d5f07862e33401d1327f4aa48fb278ad349cc15f
Francisco Rosa <chicorosa@gmail.com>
2020-02-26 16:33:57 +0000

    small profile changes, seeds done

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_093426) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -72,8 +72,8 @@ ActiveRecord::Schema.define(version: 2020_02_26_093426) do
     t.datetime "updated_at", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "address", null: false
-    t.string "phone_number", null: false
+    t.string "address"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
