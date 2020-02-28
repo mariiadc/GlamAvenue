7298d51612feab94c528a389928eea788076c1e8
Catarina Pimentel <acatarinapimentel@gmail.com>
2020-02-28 12:15:42 +0000

    fixing my show item page

--- db/migrate/20200227204850_remove_latitude_from_goods.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLatitudeFromGoods < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :goods, :latitude
-  end
-end
--- db/migrate/20200227204911_remove_longitude_from_goods.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLongitudeFromGoods < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :goods, :longitude
-  end
-end
