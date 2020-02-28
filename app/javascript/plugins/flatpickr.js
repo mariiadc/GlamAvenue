27d9efb34b08a67956e1ffee9508f1fdf460f70d
Mariia Campos <mariia.dcampos@gmail.com>
2020-02-28 16:26:21 +0000

    amazing

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,13 +1,11 @@
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
-import moment from "moment"
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+import moment from "moment";
 
 flatpickr("#start_date", {
   altInput: true,
   "plugins": [new rangePlugin({ input: "#end_date"}) ],
    minDate: moment().format("YYYY-MM-DD")
-
-
 });
 
